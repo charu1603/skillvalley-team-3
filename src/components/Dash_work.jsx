c8951f4347a9dd8574e46e0644329a41b0d39db2
Jay9874 <jayprakashsharma225@gmail.com>
2023-09-22 23:57:41 +0530

    structure of dashboard sections

---
+++ src/components/Dash_work.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export const Dash_work = () => {
+  return (
+    <div className='w-full flex flex-col min-h-screen justify-between'>
+        <section id='dashboard-top-section'>
+            <h1>quick keys</h1>
+        </section>
+        <section id='dashboard-middle-section'>
+            <h1>quick summary</h1>
+        </section>
+        <section id='dashboard-bottom-section'>
+            <h1>related recomendation</h1>
+        </section>
+
+
+    </div>
+  )
+}
