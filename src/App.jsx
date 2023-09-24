1fc98c4bdeb37b999ec544c4badce14a5a450ffb
Akash Nai <akashnai632@gmail.com>
2023-09-24 12:24:07 +0530

    Added Dashboard, Profile and UpdateProfile. and some config changes in tailwind config and eslint

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,32 @@
-import Home from './pages/Home';
-import Hero from './components/Hero';
-import WorkerD from './pages/WorkerD';
-import './App.css'
+import Home from "./pages/Home";
+import Hero from "./components/Hero";
+import WorkerD from "./pages/WorkerD";
+import "./App.css";
+import Login from "./components/Login";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Dash_work } from "./components/Dash_work";
+import { Add_job } from "./components/Add_job";
+import Dashboard from "./components/Dashboard";
+import UpdateProfile from "./components/UpdateProfile";
+import Profile from "./components/Profile";
 
 function App() {
-  
-
   return (
-    <>
-   <WorkerD />
-    
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        {/* <Route path="/" element={<Hero />}></Route> */}
+        <Route path="/" element={<WorkerD />}></Route>
+        <Route path="/dashwork" element={<Dash_work />}></Route>
+
+        {/* added by akki  */}
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/dashboard" element={<Dashboard />}></Route>
+        <Route path="/addjob" element={<Add_job />}></Route>
+        <Route path="/profile" element={<Profile />}></Route>
+        <Route path="/updateprofile" element={<UpdateProfile />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/UpdateProfile.jsx
@@ -0,0 +1,158 @@
+/*
+  This example requires some changes to your config:
+  
+  ```
+  // tailwind.config.js
+  module.exports = {
+    // ...
+    plugins: [
+      // ...
+      require('@tailwindcss/forms'),
+    ],
+  }
+  ```
+*/
+export default function Example() {
+    return (
+      <form className="space-y-6" action="#" method="POST">
+      
+  
+        <div className="bg-white px-4 py-5 shadow sm:rounded-lg sm:p-6">
+          <div className="md:grid md:grid-cols-3 md:gap-6">
+            <div className="md:col-span-1">
+              <h3 className="text-lg font-medium leading-6 text-gray-900">Personal Information</h3>
+              <p className="mt-1 text-sm text-gray-500">Use a permanent address where you can receive mail.</p>
+            </div>
+            <div className="mt-5 md:col-span-2 md:mt-0">
+              <div className="grid grid-cols-6 gap-6">
+                <div className="col-span-6 sm:col-span-3">
+                  <label htmlFor="first-name" className="block text-sm font-medium text-gray-700">
+                    First name
+                  </label>
+                  <input
+                    type="text"
+                    name="first-name"
+                    id="first-name"
+                    autoComplete="given-name"
+                    className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                  />
+                </div>
+  
+                <div className="col-span-6 sm:col-span-3">
+                  <label htmlFor="last-name" className="block text-sm font-medium text-gray-700">
+                    Last name
+                  </label>
+                  <input
+                    type="text"
+                    name="last-name"
+                    id="last-name"
+                    autoComplete="family-name"
+                    className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                  />
+                </div>
+  
+                <div className="col-span-6 sm:col-span-4">
+                  <label htmlFor="email-address" className="block text-sm font-medium text-gray-700">
+                    Email address
+                  </label>
+                  <input
+                    type="text"
+                    name="email-address"
+                    id="email-address"
+                    autoComplete="email"
+                    className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                  />
+                </div>
+  
+                <div className="col-span-6 sm:col-span-3">
+                  <label htmlFor="country" className="block text-sm font-medium text-gray-700">
+                    Country
+                  </label>
+                  <select
+                    id="country"
+                    name="country"
+                    autoComplete="country-name"
+                    className="mt-1 block w-full rounded-md border border-gray-300 bg-white py-2 px-3 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                  >
+                    <option>United States</option>
+                    <option>Canada</option>
+                    <option>Mexico</option>
+                  </select>
+                </div>
+  
+                <div className="col-span-6">
+                  <label htmlFor="street-address" className="block text-sm font-medium text-gray-700">
+                    Street address
+                  </label>
+                  <input
+                    type="text"
+                    name="street-address"
+                    id="street-address"
+                    autoComplete="street-address"
+                    className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                  />
+                </div>
+  
+                <div className="col-span-6 sm:col-span-6 lg:col-span-2">
+                  <label htmlFor="city" className="block text-sm font-medium text-gray-700">
+                    City
+                  </label>
+                  <input
+                    type="text"
+                    name="city"
+                    id="city"
+                    autoComplete="address-level2"
+                    className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                  />
+                </div>
+  
+                <div className="col-span-6 sm:col-span-3 lg:col-span-2">
+                  <label htmlFor="region" className="block text-sm font-medium text-gray-700">
+                    State / Province
+                  </label>
+                  <input
+                    type="text"
+                    name="region"
+                    id="region"
+                    autoComplete="address-level1"
+                    className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                  />
+                </div>
+  
+                <div className="col-span-6 sm:col-span-3 lg:col-span-2">
+                  <label htmlFor="postal-code" className="block text-sm font-medium text-gray-700">
+                    ZIP / Postal code
+                  </label>
+                  <input
+                    type="text"
+                    name="postal-code"
+                    id="postal-code"
+                    autoComplete="postal-code"
+                    className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+  
+       
+  
+        <div className="flex justify-end">
+          <button
+            type="button"
+            className="rounded-md border border-gray-300 bg-white py-2 px-4 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+          >
+            Cancel
+          </button>
+          <button
+            type="submit"
+            className="ml-3 inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+          >
+            Save
+          </button>
+        </div>
+      </form>
+    )
+  }
+  
